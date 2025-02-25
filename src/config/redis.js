c86ba9ba7006c8d48397feb5712fddb2ddff9634
Akkala Harish Goud <73022679+harishgoudakkala@users.noreply.github.com>
2025-02-25 21:18:15 +0530

    updated redis config

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,9 +1,14 @@
 import Redis from "ioredis"
 
 const redis = new Redis({
+  socket: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD,
+    tls: true,
+    connectTimeout: 30000,
+  },
+  maxRetriesPerRequest: null,
+  password: process.env.REDIS_PASSWORD,
 });
 
 redis.on("connect", () => {
